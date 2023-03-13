1a17934dae83306a87e134ccc4208fc17795c85d
DavidLakeT <djcardonan@eafit.edu.co>
2023-03-13 18:15:56 -0500

    Fixed docker setup for Unix environments

--- docker/service.sh
+++ docker/service.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-ROOT=$(pwd)
-DOCKER_FILE="$ROOT/docker-compose.yaml"
-export PSQL_DATA_DIR="$ROOT/data"
+set -xeuf -o pipefail
 
-if [ "$1" = "up" ]; then
+ROOT="$( readlink -f "$( dirname "${BASH_SOURCE[0]}" )" )"
+DOCKER_FILE="${ROOT}/docker-compose.yaml"
+
+export PSQL_DATA_DIR="${ROOT}/data"
+
+if [[ $1 == "up" ]]; then
     docker-compose --file "$DOCKER_FILE" up --detach
 fi
 
-if [ "$1" = "down" ]; then
+if [[ $1 == "down" ]]; then
     docker-compose --file "$DOCKER_FILE" down
 fi
