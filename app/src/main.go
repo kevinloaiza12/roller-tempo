3b16cc036e3a53e9393700eaa7331dae9d9bdf4b
Julian <jgpbak@outlook.com>
2023-03-18 23:24:36 -0500

    Implemented simple fiber app with a /users endpoint to test communication

--- app/src/main.go
+++ app/src/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	_ "github.com/lib/pq"
+	//"google.golang.org/genproto/googleapis/cloud/functions/v1"
 )
 
 const (
@@ -12,4 +16,17 @@ const (
 	DBName     = "rollertempo"
 )
 
-func main() {}
+func main() {
+	app := fiber.New()
+
+	app.Use(cors.New())
+
+	app.Get("/users", func(c *fiber.Ctx) error {
+		return c.JSON(&fiber.Map{
+			"data": "hola front, saludos desde el back",
+		})
+	})
+
+	app.Listen(":3000")
+	fmt.Println("Server listening on port 3000")
+}
