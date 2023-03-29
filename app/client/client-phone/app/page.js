627b241d6ddc5ac65dee00366cdb6b4ff271e98c
Julian <jgpbak@outlook.com>
2023-03-29 11:18:24 -0500

    Added logo for initial page

--- app/client/client-phone/app/page.js
+++ app/client/client-phone/app/page.js
@@ -1,11 +1,14 @@
 import { Inter } from 'next/font/google'
+import logo from '../public/logo.png'
+import Image from 'next/image'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main>
-      <h1>Place logo</h1>
+    <main id="welcome_page">
+      <Image id="logo"src={logo}></Image>
+      <h1 id="logo_title">Roller Tempo</h1> 
     </main>
   )
 }
