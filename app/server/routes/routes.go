9206530bcf7c26205fa6d499ff74bc757f7e9594
Julian <jgpbak@outlook.com>
2023-05-13 14:26:40 -0500

    Added feature to remove turn, and organized urls in routes

--- app/server/routes/routes.go
+++ app/server/routes/routes.go
@@ -9,27 +9,27 @@ import (
 
 func RegisterAttractionRoutes(app *echo.Echo, ctx context.Context, controller *controller.AttractionController) {
 	app.GET("/api/attractions", controller.Attractions)
+	app.POST("/api/attractions/register", controller.CreateNewAttraction)
 	app.GET("/api/attractions/:id", controller.GetAttractionByID)
 	app.GET("/api/attractions/:id/turns", controller.GetNextRoundTurns)
 
-	app.POST("/api/attractions/register", controller.CreateNewAttraction)
 }
 
 func RegisterRewardRoutes(app *echo.Echo, ctx context.Context, controller *controller.RewardController) {
 	app.GET("/api/rewards", controller.Rewards)
+	app.POST("/api/rewards/register", controller.CreateNewReward)
 	app.GET("/api/rewards/:id", controller.GetRewardByID)
 
-	app.POST("/api/rewards/register", controller.CreateNewReward)
 }
 
 func RegisterUserRoutes(app *echo.Echo, ctx context.Context, controller *controller.UserController) {
 	app.GET("/api/users", controller.Users)
-	app.GET("/api/users/:id", controller.GetUserByID)
-
 	app.POST("/api/users/register", controller.CreateNewUser)
 	app.POST("/api/users/turn", controller.UpdateUserTurn)
+
 	app.POST("/api/users/:id/reward", controller.RewardUser)
 	app.POST("/api/users/:id/penalize", controller.PenalizeUser)
+	app.GET("/api/users/:id", controller.GetUserByID)
 
 	app.PUT("/api/users/:id/removeturn", controller.RemoveUserTurn)
 }
