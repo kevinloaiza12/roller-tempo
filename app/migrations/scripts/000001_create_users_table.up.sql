fea3ed8b4f4ae5f3296cddf4491d8701f6bea32a
DavidLakeT <djcardonan@eafit.edu.co>
2023-05-27 15:02:47 -0500

    Major tweaks to structure

--- app/migrations/scripts/000001_create_users_table.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-  UserID                 INT NOT NULL PRIMARY KEY,
-  UserCoins              INT DEFAULT 0,
-  UserTurn               INT DEFAULT 0,
-  UserCurrentAttraction  VARCHAR(255) DEFAULT ''
-);
